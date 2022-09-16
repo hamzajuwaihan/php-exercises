d7cddfa65d60266589b4b7d07a4a3f381447f616
Hamza <108789747+hamzajuwaihan@users.noreply.github.com>
2022-09-16 18:21:39 +0300

    task 2

---
+++ search.php
@@ -0,0 +1,15 @@
+<form action="" method="get">
+    <p>put the link you want:</p>
+    <input type="text" name="link" id="">
+    <input type="submit" value="Go">
+
+
+</form>
+
+<?php 
+if (isset($_GET['link'])) {
+    header("Location: ".$_GET['key']);
+    die();
+}
+
+?>
